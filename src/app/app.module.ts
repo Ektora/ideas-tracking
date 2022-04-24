2a1ade34aee4e82ec1cd1aa3b65aad679695b8d5
Ektora <zsoltlucski@gmail.com>
2022-04-24 20:42:27 +0200

    Angular material bekötése + login screen stílusozása + auth service váza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     CoreModule,
     SharedModule,
-    AuthModule
+    AuthModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { User } from './models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private _currentUser = new BehaviorSubject<User>(undefined);
 
   constructor() { }
+
+  login(email: string, password: string){
+
+  }
+
+  logout(){
+
+  }
+
+  isLoggedIn(){
+    return this._currentUser.getValue() !== undefined;
+  }
+
+  get currentUser() {
+    return this._currentUser.asObservable();
+  }
 }
