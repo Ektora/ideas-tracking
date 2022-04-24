aeab63b27de10f4d40a4f8b1e8311ffd074b23b0
Ektora <zsoltlucski@gmail.com>
2022-04-24 10:33:12 +0200

    Routing kialakítása

---
+++ src/app/ideas/ideas-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListIdeaComponent } from './list-idea/list-idea.component';
+import { NewIdeaComponent } from './new-idea/new-idea.component';
+
+const routes: Routes = [
+  {
+    path : '',
+    component: ListIdeaComponent
+  },
+  {
+    path: 'new',
+    component: NewIdeaComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class IdeasRoutingModule { }
